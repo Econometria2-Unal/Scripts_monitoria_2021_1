c16d73e872181b73e9d489b3ae0599493135c076
gecrodriguezpe <gecrodriguezpe@unal.edu.co>
2020-09-16 16:58:53 -0500

    modificaciones extensas. Agregar varias carpetas a monitoria1 y monitoria2. Agregue PDF para la monitoria 2

--- Sesión 2 de Monitoria  - Cargar datos, MCO y Supuestos/3_códigos/Script 2 - Importación de datos y MCO.R
+++ Sesión 2 de Monitoria  - Cargar datos, MCO y Supuestos/3_códigos/Script 2 - Importación de datos y MCO.R
@@ -1,14 +1,14 @@
 ##______________________________________________________________________________
 ##______________________________________________________________________________
-#       UNIVERSIDAD NACIONAL DE COLOMBIA - FACULTAD DE CIENCIAS ECON”MICAS
+#       UNIVERSIDAD NACIONAL DE COLOMBIA - FACULTAD DE CIENCIAS ECON?MICAS
 #                         ECONOMETRIA II - 2020-II
-#                 SESI”N #2 MONITORIA : ESTADÕSTICA B¡SICA
+#         SESI√ìN #2 MONITORIA : ESTAD√çSTICA B√ÅSICA y  REGRESI√ìN LINEAL
 ##______________________________________________________________________________
 ##______________________________________________________________________________
 
 rm(list = ls()) #limpiar el environment 
 
-## 1. ImportaciÛn y tipos de datos ____________________________________________________
+## 1. Importaci√≥n y tipos de datos ____________________________________________________
 
 ##| Formatos  Permitidos |            Tipo             |   |   |   |
 ##|----------------------|-----------------------------|---|---|---|
@@ -17,7 +17,7 @@ rm(list = ls()) #limpiar el environment
 ##|          dta         |            Stata            |   |   |   |
 ##|       xls, xlsx      |            Excel            |   |   |   |
 
-#1.1 ConfiguraciÛn del directorio de trabajo---------------------------------------------
+#1.1 Configuraci√≥n del directorio de trabajo---------------------------------------------
 
 directorio = getwd()       #Obtener el directorio de trabajo 
 setwd(directorio)
@@ -28,12 +28,12 @@ setwd(directorio)
 #1.2 Importar formato csv----------------------------------------------------------------
 #read.csv
 titanic<-read.csv(file.choose(),sep=",")
-View(titanic)   #La funciÛn View es con V may˙scula
+View(titanic)   #La funci√≥n View es con V may√∫scula
 attach(titanic) # Trabajar con el nombre de los datos de las variables 
-summary(titanic)  #res˙men de los datos
+summary(titanic)  #res√∫men de los datos
 
 #1.3 Archivos de Excel--------------------------------------------------------------------
-install.packages("readxl")
+#install.packages("readxl")
 library("readxl")
 
 #Cargos Datos1
@@ -45,55 +45,57 @@ View(Excel)
 # 1.4 Datos en STATA ---------------------------------------------------------------------
 # datos en http://qcpages.qc.cuny.edu/~rvesselinov/statadata/phillips.dta
 
-install.packages("foreign") 
+#install.packages("foreign") 
 library(foreign)
 
 #read.dta
 Datos<-read.dta("http://qcpages.qc.cuny.edu/~rvesselinov/statadata/phillips.dta")
 
-
-#ImportciÛn de datos usando las biblioteas de tidyverse----------------------------------
+#Importaci√≥n de datos usando las bibliotecas de tidyverse----------------------------------
 
 #Instalamos y cargamos los paquetes.
-install.packages("tidyverse")
+#install.packages("tidyverse")
 library(tidyverse)
 
 # La biblioteca tidyverse es un conjunto de paquetes de R que permiten: 
 # Importar, modificar y analizar bases de datos 
-# Contiene el objeto: tibble que cumple el mismo papel que un dataframe pero con m·s funcionalidades
-# Contiene adem·s funcionalidades de gr·ficaciÛn
+# Contiene el objeto: tibble que cumple el mismo papel que un dataframe pero con m√°s funcionalidades
+# Contiene adem√°s funcionalidades de graficaci√≥n
 # Paquetes principales:
 #### dplyr: Para modificar los datos dentro de la base de datos
 #### tidyr: Para modificar la estructura de la base de datos
-#### ggplot2: Biblioteca de graficaciÛn
+#### ggplot2: Biblioteca de graficaci?n
 #### readr: Para importar y trabajar con archivos excel 
 
 # Un ejemplo de esto es importar un archivo csv usando tidyverse
 # read_csv importa la base de datos como un objeto tibble
-titanic2 = read_csv(file.choose()) # La funciÛn read_csv es diferente a read.csv
+titanic2 = read_csv(file.choose()) # La funci?n read_csv es diferente a read.csv
 # glimpse es una herramienta que permite visualizar la base de datos en la consola
 glimpse(titanic2) # funciona mejor si se emplea sobre un objeto de tipo tibble
 
 
-# 2. ManipulaciÛn de base de datos ______________________________________________________
+# 2. Manipulaci√≥n de base de datos ______________________________________________________
 
 # 2.1 Convertir variables cualitativas en cuantitativas -----------------------------------------------
 
-titanic$Sex<-ifelse(titanic$Sex=="female",1,0) #ifelse(test lÛgico, yes/verdadero, no/falso)
+titanic$Sex<-ifelse(titanic$Sex=="female",1,0) #ifelse(test l√≥gico, yes/verdadero, no/falso)
 titanic$PClass<-ifelse(titanic$PClass=="1st",1,
-                       ifelse(titanic$PClass=="2nd",2,3))
+                       ifelse(titanic$PClass=="2nd",2,3)) #ifelse anidados
 
 # 2.2 Medidas de tendencia central y variabilidad-----------------------------------------
 
 mean(Age, na.rm= T)         #Media
 median(Age,na.rm = T)       #Mediana
-sd(Age,na.rm = T)           #DesviaciÛn estandar
-var(Age,na.rm = T)          #Varianza
-help(mean) #na.rm es para que las casillas en las que no hay informaciÛn no se tomen en cuenta
+sd(Age,na.rm = T)           #Desviaci√≥n estandar muestral
+var(Age,na.rm = T)          #Varianza muestral
+help(mean) #na.rm es para que las casillas en las que no hay informaci?n no se tomen en cuenta
 
-#2.3 Gr·ficos----------------------------------------------------------------------------
+# Si la variable a la que se le aplica la medidad de tendencia central contiene un na es necesario o 
+# llenar los valores faltantes o usar na.rm para eliminar los missing values
 
-# 2.3.1 Gr·ficos usando las funciones de R
+#2.3 Gr√°ficos----------------------------------------------------------------------------
+
+# 2.3.1 Gr√°ficos usando las funciones de R
 
 #Tablas
 table(Sex)
@@ -101,7 +103,7 @@ table(PClass)
 
 #Barras barplot
 x11()
-barplot(table(Sex), main ="Frecuencia de gÈnero", xlab="GÈnero", ylab ="N˙mero", col="brown")
+barplot(table(Sex), main ="Frecuencia de g?nero", xlab="G?nero", ylab ="N?mero", col="brown")
 
 #histograma hist
 x11()
@@ -113,16 +115,16 @@ boxplot(Age, col=c(3), horizontal = TRUE, main="Edad tripulantes Titanic")
 abline(v=mean(Age,na.rm = T),col=c(2))
 summary(Age)
 
-#Gr·fico tipo pie
+#Gr√°fico tipo pie
 x11()
-pie(table(Survived),radius=0.8, main = "Gr·fico tipo pastel", col = c("brown1","blue2"))
+pie(table(Survived),radius=0.8, main = "Gr√°fico tipo pastel", col = c("brown1","blue2"))
 legend("topleft", legend= c("Desaparecido","Sobreviviente"), pch = 10,col =
          c("brown1","dodgerblue"),)
 
 
-# 2.2 Gr·ficos usando la biblioteca ggplot2
+# 2.2 Gr√°ficos usando la biblioteca ggplot2
 
-# La funciÛn base para hacer todos los gr·ficos es ggplot
+# La funci√≥n base para hacer todos los gr√°ficos es ggplot
 hist1 = ggplot(data = titanic, aes(Age)) +
         geom_histogram(fill = "green") + 
         labs(title = "Edad de los tripulantes") +
@@ -130,18 +132,18 @@ hist1 = ggplot(data = titanic, aes(Age)) +
         ylab("Frecuencia") 
 hist1
 
-#3. REGRESI”N LINEAL ____________________________________________________________________
+#3. REGRESI√ìN LINEAL ____________________________________________________________________
 
-install.packages("wooldridge")
+#install.packages("wooldridge")
 library(wooldridge)
 
 #Importar base de datos llamada bwght2
 
 data("bwght2") # para importar bases de datos que se encuentran en el paquete wooldridge
 attach(bwght2)
-help(bwght2) # descripciÛn de la base de datos 
+help(bwght2) # descripci√≥n de la base de datos 
 
-#DESCRIPCI”N:  N=1832 & 23 variables, cross-sectional individual data on birth weights
+#DESCRIPCI√ìN:  N=1832 & 23 variables, cross-sectional individual data on birth weights
 
 ##VARIABLES:
 #mage: mother's age, years
@@ -168,58 +170,56 @@ help(bwght2) # descripciÛn de la base de datos
 #magesq: mage^2
 #npvissq: npvis^2
 
-#CARACTERÕSTICAS DE LAS VARIABLES
-summary(bwght2) # informaciÛn m·s extensa de la base de datos 
+#CARACTER√çSTICAS DE LAS VARIABLES
+summary(bwght2) # informaci√≥n m√°s extensa de la base de datos 
 glimpse(bwght2) # manera compacta de visualizar la base de datos
 
 #Histograma de la variable dependiente. 
 x11()
 hist(bwght2$bwght)
 
-###ESTIMACI”N
+###ESTIMACI√ìN
 
-#El peso promedio de un niÒo al nacer es una funciÛn de: el promedio de cigarrillos fumados por la madre, promedio
-#del consumo de alcohol, el sexo del bebÈ, la edad de la madre y de la raza de la madre.
+#El peso promedio de un ni√±o al nacer es una funci√≥n de: el promedio de cigarrillos fumados por la madre, promedio
+#del consumo de alcohol, el sexo del bebe, la edad de la madre y de la raza de la madre.
 MODELO1 = lm(bwght ~ cigs + drink + npvis + male + mage + mwhte + mblck, data = bwght2)
 summary(MODELO1)
 
-#En esta regresiÛn, a parte de las anteriores variables, controlamos por la educaciÛn del padre y de la madre, pues
-#es posible que al no incluirlas en la regresiÛn se presente un sesgo de variable omitida: øPuede haber correlaciÛn 
+#En esta regresi√≥n, a parte de las anteriores variables, controlamos por la educaci?n del padre y de la madre, pues
+#es posible que al no incluirlas en la regresi?n se presente un sesgo de variable omitida: ?Puede haber correlaci?n 
 #entre el consumo de alcohol y cigarrillos con el nivel educativo de la madre y el padre?
 MODELO2 = lm(bwght ~ cigs + drink + npvis + male + mage + magesq +mwhte + mblck + meduc + feduc , data = bwght2)
 summary(MODELO2)
 
-#A partir del mismo argumento utilizaremos el promedio de la educaciÛn de ambos padres, con objeto de reducir las contradicciones
+#A partir del mismo argumento utilizaremos el promedio de la educaci?n de ambos padres, con objeto de reducir las contradicciones
 #de resultados que puedan darse entre el nivel educativo del padre y de la madre por separados.
 mean.educ = (meduc + feduc)/2
 MODELO3 = lm(bwght ~ cigs + drink + npvis + male + mage + magesq +mwhte + mblck + mean.educ, data = bwght2)
 summary(MODELO3)
 
+## PRESENTACI√ìN DE RESULTADOS
 
-## PRESENTACI”N DE RESULTADOS
-
-# Stargazer permite crear autom·ticamente tablas de regresiones y estadÌstica descriptiva de alta calidad
+# Stargazer permite crear autom√°ticamente tablas de regresiones y estad√≠stica descriptiva de alta calidad
 
 install.packages("stargazer")
 library(stargazer)
 
-# FunciÛn b·sica del paquete stargazer
+# Funci√≥n b√°sica del paquete stargazer
 stargazer(MODELO1, MODELO2, MODELO3, type="text", column.labels = c("REG1","REG2", "REG3"),keep.stat = c("n", "rsq","adj.rsq","aic"))
-# En el ejemplo stargazer mostrar· las 3 regresiones lineales presentadas.
-
+# En el ejemplo stargazer mostrar√° las 3 regresiones lineales presentadas.
 
 # Opciones de Stargazer: 
 ## Permite visualizar varios modelos al mismo tiempo
-## title: introducir un tÌtulo
+## title: introducir un t?tulo
 ## type: el formato de la tabla (e.g. text o latex)
 ## omit: si se quiere omitir alguna variable de la tabla
-## style: especÌfica el estilo de la tabla (e.g. aer para American Economic Review)
+## style: espec?fica el estilo de la tabla (e.g. aer para American Economic Review)
 
 
 #----------------------
 ##OTRO EJEMPLO
 
-#DESCRIPCI”N:  173 observations on 10 variables.
+#DESCRIPCI√ìN:  173 observations on 10 variables.
 
 #state: state postal code
 #district: congressional district
@@ -233,7 +233,7 @@ stargazer(MODELO1, MODELO2, MODELO3, type="text", column.labels = c("REG1","REG2
 #lexpendB: log(expendB)
 #shareA: 100*(expendA/(expendA+expendB))
 
-#DESCRIPCI”N DE LAS VARIABLES.
+#DESCRIPCI√ìN DE LAS VARIABLES.
 
 #Cargamos la base de datos contenida en el archivo de excel Elecciones
 library(readxl)
@@ -246,138 +246,97 @@ attach(Elecciones)
 View(Elecciones)
 summary(Elecciones)
 
-#3.1 EstimaciÛn -------------------------------------------------------------------------
+#3.1 Estimaci√≥n -------------------------------------------------------------------------
 
-#Realizamos una regresiÛn nivel-nivel
-RegresiÛnA1 = lm(voteA ~ democA +expendA + expendB + prtystrA) #lin-lin
-summary(RegresiÛnA1)
+#Realizamos una regresi√≥n nivel-nivel
+RegresionA1 = lm(voteA ~ democA +expendA + expendB + prtystrA) #lin-lin
+summary(RegresionA1)
 
-#Realizamos una regresiÛn nivel-logaritmo.
-RegresiÛnA2 = lm(voteA ~ democA +log(expendA)+log(expendB) + log(prtystrA)) #lin-log
-summary(RegresiÛnA2)
+#Realizamos una regresi√≥n nivel-logaritmo.
+RegresionA2 = lm(voteA ~ democA +log(expendA)+log(expendB) + log(prtystrA)) #lin-log
+summary(RegresionA2)
 
-#PresentaciÛn de resultados
+#Presentaci√≥n de resultados
 library(stargazer)
-stargazer(RegresiÛnA1, RegresiÛnA2, type="text", column.labels = c("N-N","N-L"),keep.stat = c("n", "rsq" "adj.rsq", "aic"))
+stargazer(RegresionA1, RegresionA2, type="text", column.labels = c("N-N","N-L"),keep.stat = c("n", "rsq", "adj.rsq", "aic"))
 
 #3.2 Pruebas de significancia individual para las variables ------------------------------
 
 install.packages("lmtest")
 library("lmtest")
-coeftest(RegresiÛnA2)
+coeftest(RegresionA2)
 
-#Intervalos de confianza para los coeficientes de la regresiÛn
-confint(RegresiÛnA2) #Al 95%
-confint(RegresiÛnA2, level = 0.90) #Al 90%
+#Intervalos de confianza para los coeficientes de la regresi?n
+confint(RegresionA2) #Al 95%
+confint(RegresionA2, level = 0.90) #Al 90%
 
 #Valores ajustados/estimados (fitted.values)
-fitted.values(RegresiÛnA2)
+fitted.values(RegresionA2)
 
-#Residuales de la regresiÛn
-residuals(RegresiÛnA2)
-rstandard(RegresiÛnA2) #Residuales estandarizados (divididos por su desviaciÛn est·ndar)
+#Residuales de la regresi√≥n
+residuals(RegresionA2)
+rstandard(RegresionA2) #Residuales estandarizados (divididos por su desviaci√≥n est√°ndar)
 
-# 4. ValidaciÛn de supuestos ------------------------------------------------------------
+# 4. Validaci√≥n de supuestos ------------------------------------------------------------
 
-#Test de Ramsey para especificaciÛn errÛnea
-resettest(RegresiÛnA2) #Ho = el modelo est· bien especificado
+#Test de Ramsey para especificaci√≥n err√≥nea
+resettest(RegresionA2) #Ho = el modelo est? bien especificado
 
 #Heterocedasticidad en los residuales
 install.packages("car")
 library("car")
 
-bptest(RegresiÛnA2) #Test Breusch-Pagan con Ho=Homocedasticidad
+bptest(RegresionA2) #Test Breusch-Pagan con Ho=Homocedasticidad
 
-ncvTest(RegresiÛnA2)#Test de Varianza no constante con Ho= Homocedasticidad
+ncvTest(RegresionA2)#Test de Varianza no constante con Ho= Homocedasticidad
 
-#CorrelaciÛn serial en los residuales
+#Correlaci√≥n serial en los residuales
 
-dwtest(RegresiÛnA2)#Durbin Watson test (Ho:No autocorrelaciÛn de 1er orden)
+dwtest(RegresionA2)#Durbin Watson test (Ho:No autocorrelaci√≥n de 1er orden)
 
-bgtest(RegresiÛnA2)#Prueba Breush-Godfrey (Ho:No autocorrelaciÛn de orden p)
+bgtest(RegresionA2)#Prueba Breush-Godfrey (Ho:No autocorrelaci√≥n de orden p)
 
-#Gr·fico de correlaciÛn serial
+#Gr√°fico de correlaci√≥n serial
 
-residA2 =rstandard(RegresiÛnA2)
+residA2 =rstandard(RegresionA2)
 acf(residA2, xlab="Residuos", ylab="Autocorrelaciones", main= "CORRELALOGRAMA")
 
-#ErrÛres robustos a la heterocedasticidad y correlaciÛn con HAC
-install.packages("sandwich")
+#Errores robustos a la heterocedasticidad y correlaci√≥n con HAC
+#install.packages("sandwich")
 library("sandwich")
 
-vcovHC(RegresiÛnA2)
-coeftest(RegresiÛnA2, vcov=vcovHC(RegresiÛnA2))
-coeftest(RegresiÛnA2)
+vcovHC(RegresionA2)
+coeftest(RegresionA2, vcov=vcovHC(RegresionA2))
+coeftest(RegresionA2)
 
 #Normalidad en los residuales con Ho = Normalidad
-install.packages("tseries")
+#install.packages("tseries")
 library("tseries")
 
-resreg=residuals(RegresiÛnA2) # Calculamos los residuos 
+resreg=residuals(RegresionA2) # Calculamos los residuos 
 
 shapiro.test(resreg) #Test Shapiro Wilk (Ho= normalidad)
-jarque.bera.test(resreg)#Test Jarque Bera  (#Ho= normalidad)/M·s apropiado para series de tiempo
+jarque.bera.test(resreg)#Test Jarque Bera  (#Ho= normalidad)/M√°s apropiado para series de tiempo
  
 
-#Histograma de los residuos
-hist(resreg, freq=FALSE, main="DistribuciÛn de los Residuales", breaks = 20, prob=TRUE)
+#Histograma de los residuales
+
+hist(resreg, freq=FALSE, main="Distribuci√≥n de los Residuales", breaks = 20, prob=TRUE)
 curve(dnorm(x, mean=mean(resreg), sd=sd(resreg)),col="darkblue", lwd=2, add=TRUE)
 
-#Gr·fica qq-plot
-install.packages("car")
+#Gr√°fica qq-plot
+
+qqnorm(resreg)
+qqline(resreg)
+
+#install.packages("car")
 library(car)
-install.packages("patchwork") #Con este paquete podemos unificar gr·ficas del mismo formato
-library(patchwork)
-install.packages("gridGraphics")
-library(gridGraphics)
-install.packages("tibble")
-library(tibble)
-install.packages("ggplot2")
-library(ggplot2)
-
-#Definimos los cuartiles muestrales
-q <- (rank(resreg) - .5) / length(resreg)
-q
-
-
-#Definimos los intervalos de confianza
-band <- tibble(
-        z = seq(-2.2, 2.2, length.out = 300),
-        n = length(d$x_sample),
-        
-        sample_sd = sd(d$x_sample),
-        se = sample_sd * se_z(z, n),
-        line = mean(d$x_sample) + sample_sd * z,
-        upper = line + 2 * se,
-        lower = line - 2 * se,
-        
-        robust_sd = IQR(d$x_sample) / 1.349,
-        robust_line = median(d$x_sample) + z * robust_sd,
-        robust_se =  robust_sd * se_z(z, n),
-        robust_upper = robust_line + 2 * robust_se,
-        robust_lower = robust_line - 2 * robust_se,
-)
-
-#Ahora graficamos utilizando la funciÛn qqplot
-
-#Fijamos los m·rgenes
-par(mar = c(4, 2, 1, 2))
-
-p1 <- wrap_elements(~ car::qqPlot(resreg))
-
-p2 <- wrap_elements(~ {
-        car::qqPlot(resreg)
-        lines(band$z, band$robust_line, col = "black", lwd = 2)
-        lines(band$z, band$robust_upper, col = "black", lwd = 2)
-        lines(band$z, band$robust_lower, col = "black", lwd = 2)
-})
-
-p1 + p2
+qqPlot(resreg)
 
 #__________________________________________________________
 
 #Nota:
-#øQuÈ elementos incluir en el stargazer?
+#¬øQu√© elementos incluir en el stargazer?
 
 #"all" all statistics
 #"adj.rsq" adjusted R-squared
