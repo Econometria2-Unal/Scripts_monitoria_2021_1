c28df70265c582c7517e7ec83d73368ccc8b4330
gecrodriguezpe <gecrodriguezpe@unal.edu.co>
2021-08-21 19:26:43 -0500

    Modificaciones después de la monitoria del día 21 de Agosto 2021

--- Sesión 10 de Monitoria - DatosPanel2/3_códigos/Sesión 10 - Panel con pruebas.R
+++ Sesión 10 de Monitoria - DatosPanel2/3_códigos/Sesión 10 - Panel con pruebas.R
@@ -1,8 +1,8 @@
 ##______________________________________________________________________________
 ##______________________________________________________________________________
 #       UNIVERSIDAD NACIONAL DE COLOMBIA - FACULTAD DE CIENCIAS ECON?MICAS
-#                         ECONOMETRIA II - 2020-II
-#                 SESIÓN #4 MONITORIA : Modelos para datos panel.
+#                         ECONOMETRIA II - 2021-I
+#                 SESIÓN #10 MONITORIA : Modelos para datos panel.
 ##______________________________________________________________________________
 ##______________________________________________________________________________
 
@@ -11,31 +11,42 @@ rm(list = ls()) #limpiar el enviroment
 #Paquetes 
 # install.packages("plm")         #Panel linear models 
 # install.packages("gplots")      #Tools for plotting data 
-# install.packages("stargazer")   #Tablas m?s est?ticas 
-# install.packages("foreing")     #Importar datos 
+# install.packages("stargazer")   #Tablas más estéticas 
+# install.packages("haven")       #Importar datos 
 # install.packages("sandwich")    #Estimador de erores robustos 
-# install.packages("lmtest")
-# install.packages("tseries")
-# install.packages("wooldridge")  
+# install.packages("lmtest")      #Provee herramientas para hacer inferencia estadística para modelos paramétricos lineales
+# install.packages("tseries")     
+# install.packages("wooldridge")  # Contiene data sets con los que se pueden realizar ejercicios econométricos
 # install.packages("tidyverse")
  
 library(plm);library(gplots);library(stargazer)
-library(foreign);library(sandwich);library(lmtest)
+library(haven);library(sandwich);library(lmtest)
 library(tseries);library(wooldridge);library(tidyverse)
 
+# Nota: Foreign es una biblioteca que es muy freuentemente usada para importar bases
+#       de datos de stata (.dta es el archivo de base de datos de stata)
+#       No obstante, dicho paquete genera inconvenientes a la hora de importar cierto
+#       bases de datos provenientes de ciertas versiones de stata
+#       El paquete haven es mucho más general que el paquete foreign y no genera 
+#       inconvenientes a la hora de importar bases de datos tipo .dta. Por tanto, 
+#       al igual que en el presente script, se recomienda usar la función read_dta a la
+#       hora de trabajar bases de datos de stata. 
 
-#Base de datos: Wooldridge 14.4 ?Ha cambiado la educaci?n a lo largo del tiempo?
-Education=read.dta("http://fmwww.bc.edu/ec-p/data/wooldridge/wagepan.dta")  
+#Base de datos: Wooldridge 14.4 ¿Ha cambiado la educación a lo largo del tiempo?
+Education=read_dta("http://fmwww.bc.edu/ec-p/data/wooldridge/wagepan.dta")  
 attach(Education)
+# Manera de visualizar toda la base de datos desde R
 View(Education)
+# Manera compacta de ver una base de datos
+glimpse(Education)
 
 #Tratar la base de datos como un panel de datos
 panel.Education = pdata.frame(Education, index = c("nr","year"))
 help("pdata.frame")
 
- #Estructura del codigo ---------------------------------------------------#
- #Nombre <- pdata.frame(Base_datos, index=c("Var_indivuo","Var_tiempo"))
- #-------------------------------------------------------------------------#
+#Estructura del codigo ---------------------------------------------------#
+#Nombre <- pdata.frame(Base_datos, index=c("Var_indivuo","Var_tiempo"))
+#-------------------------------------------------------------------------#
 
 #Para conocer las dimensiones del panel
 pdim(panel.Education)
@@ -43,16 +54,16 @@ pdim(panel.Education)
 #Para determinar si las variables cambian a lo largo del tiempo
 pvar(panel.Education)
 
-#Distribuci?n de la poblaci?n.
-plotmeans(lwage ~ married, main="Distribuci?n de la poblaci?n", data=panel.Education)
+#Distribución de la población.
+plotmeans(lwage ~ married, main="Distribución de la población", data=panel.Education)
 
-#Definici?n de las variables
+#Definición de las variables
 exper2 = expersq
-panel.Education$yr = factor(panel.Education$year)#Una Dummy para cada a?o de la muestra
+panel.Education$yr = factor(panel.Education$year)#Una Dummy para cada año de la muestra
 
-#Estimaci?n MCO combinados -E.F.- E.A.----------------------------------------------------
+#Estimación MCO combinados -E.F.- E.A.----------------------------------------------------
 
-#M?nimos cuadrados combinados.
+#Mínimos cuadrados combinados.
 Pooled = plm(lwage~educ+black+hisp+exper+exper2+married+union+yr,
              data=panel.Education, model="pooling")
 summary(Pooled)
@@ -72,21 +83,21 @@ FD = plm(lwage~exper2+married+union+yr,
             data=panel.Education, model="fd")
 summary(FD)
 
-#Presentaci?n de resultados.
+#Presentación de resultados.
 stargazer(Pooled,Random,Fixed,FD, type="text",           
           column.labels=c("OLS","RE","FE", "PD", "BT"),keep.stat=c("n","rsq"))
 
 ## NOTA ACLARATORIA SOBRE BETWEEN ------------------------------------------------## 
-## El modelo between en plm realiza una transformaci?n diferente a la que vieron  
+## El modelo between en plm realiza una transformación diferente a la que vieron  
 ## en clase, pues tiene en cuenta las variaciones que puedan existir entre grupos 
-## de individuos. Una anotaci?n importante es que con este tipo de modelos se
+## de individuos. Una anotación importante es que con este tipo de modelos se
 #pierden obervaciones. 
 
 ## Mientras que el modelo de efectos fijos o "within" tiene en cuenta 
 ## las variaciones de los sujetos y no entre sujetos. 
 
 ## En otras palabras, "within" usa la media de cada individuo, mientras que "between" 
-## usa la media de todos los datos para un a?o especifico.
+## usa la media de todos los datos para un año especifico.
 
 ## Para evaluar efectos especificos en los modelos que estimen con PLM, puden usar 
 ## el argumento " effect" y este permite "individual" , "time", "twoways"
@@ -95,14 +106,14 @@ stargazer(Pooled,Random,Fixed,FD, type="text",
 
 #Selección del modelo ---------------------------------------------------------------------
 
-#?es mejor Efectos fijos o Efectos Aleatorios?
+#¿es mejor Efectos fijos o Efectos Aleatorios?
 #Test de Hausman para comparar EF vs EA
 phtest(Fixed, Random) #Ho: Los modelos son equivalentes estadisticamente, por eficiencia elijo EA. 
 
 #Test de Primeras diferencias de Wooldridge para comparar EF vs PD
 pwfdtest(lwage~exper2+married+union+yr, data=panel.Education,h0= "fe") #H0 = corr(Uit,Uit-1) = 0
 pwfdtest(lwage~exper2+married+union+yr, data=panel.Education,h0= "fd") #H0 = errores diferenciados no correlacionados
-#La prueba no es concluyente, tanto los err?res diferenciados como sin diferenciar tienen correlaci?n serial. 
+#La prueba no es concluyente, tanto los errores diferenciados como sin diferenciar tienen correlación serial. 
 
 #Pooled VS Efectos Aleatorios
 #Prueba de Multiplicadores de Lagrange de Breusch-Pagan para E.A
@@ -112,13 +123,13 @@ plmtest(Pooled,type = "bp")  #Ho:Mejor Pooled porque var(ai)=0
 #Prueba Breush-Pagan ( Ho: Homocedasticidaad) 
 bptest(Pooled) # si p-value>5% posiblemente es Pooled  
 
-# ?El efecto es individual, temporal, o ambos?
+# ¿El efecto es individual, temporal, o ambos?
 plmtest(Pooled,"time","bp")     #Ho:Efectos temporales no significativos 
 plmtest(Pooled,"individual","bp")     #Ho:Efectos indivuduales no significativos
 plmtest(Pooled,"twoways","bp")     #Ho:Efectos temporales e individuales no significativos
 
 
-#Validaci?n de supuestos------------------------------------------------------------------
+#Validación de supuestos------------------------------------------------------------------
 
 #Prueba de heterocedasticidad
 bptest(Pooled);bptest(Random);bptest(Fixed); bptest(FD)
@@ -130,15 +141,15 @@ bgtest(Pooled);bgtest(Random);bgtest(Fixed);bgtest(FD)
 pcdtest(Pooled,test = "lm");pcdtest(Random,test = "lm");pcdtest(Fixed,test = "lm");#pcdtest(FD,test = "lm")      
 
 #Correci?n de correlaci?n serial para EF.
-MCOV=vcovHC.plm(Fixed, method=c("arellano"))          
+# MCOV=vcovHC.plm(Fixed, method=c("arellano"))          
 MCOV1=vcovHC(Fixed, method="arellano")
-coeftest(Fixed,MCOV)
+# coeftest(Fixed,MCOV)
 coeftest(Fixed,MCOV1)
 help("vcovHC.plm")
          # Arellano : Soluciona heterosedasticidad y autocorrelacion serial 
          # White1 - Whit 2 : Soluciona heteroscedaticidad. 
 
-#An?lisis de Normalidad.
+#Análisis de Normalidad.
 hist(residuals(Fixed))
 
 qqnorm(residuals(Fixed))
@@ -155,8 +166,8 @@ jarque.bera.test(residuals(Fixed))
 #OTRO EJEMPLO:----------------------------------------------------------------------------
 
 #En este ejercicio se utiliza la base de datos JTRAIN.RAW para determinar el efecto del
-#subsidio a la capacitaci?n laboral en las horas de capacitaci?n por empleado. El modelo 
-#b?sico para los tres a?os es: 
+#subsidio a la capacitaci?n laboral en las horas de capacitación por empleado. El modelo 
+#básico para los tres años es: 
 #hrsemp~ Bo + S1d88 + S2d89+ B1grant + B2grant_1 + B3lemploy + ai + uit
 
 #Utilizaremos la base de datos Jtrain
